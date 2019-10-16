e34924d394c18e7771b439315dec9a4f20c2454f
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-10-16 15:36:24 +0200

    Update est_fod

--- est_fod.sh
+++ est_fod.sh
@@ -1,75 +1,27 @@
 #!/bin/sh
-# Usage: est_fod dwi_preproc.nii.gz bvecs bvals
 
-# Uses input from preproc.sh
+# Usage: ./est_fod subjectID
+# Example: ./est_fod 917255
 
-# Pipeline references:
-# https://osf.io/ht7zv/ (Tutorial)
-# https://mrtrix.readthedocs.io/en/latest/
+dir="subjects/$1"
 
-# Check brain mask
-# mrview dwi_denoised_pe_corr.mif -overlay.load dwi_brain_mask.mif
+dwi2response \
+tournier \
+"${dir}/data_input.nii.gz" \
+"${dir}/response.txt" \
+-mask "${dir}/dwi_brain_mask.nii.gz" \
+-fslgrad "${dir}/bvecs_input" "${dir}/bvals_input" &&
 
-# Check response estimation:
-# mrview dwi_denoised_pe_corr.mif -overlay.load response_voxels.mif
-# shview response.txt
-
-# Check fods with WM mask
-# mrview wm.nii.gz -odf.load_sh fod_norm.mif
-
-
-################################################################################
-
-# Extract b=0 and b=1000 volumes
-dwiextract $1 dwi_subset.mif \
--fslgrad $2 $3 \
--shells 0,1000 \
--force &&
-
-
-# Brain Mask
-dwi2mask dwi_subset.mif dwi_brain_mask.mif \
--force &&
-
-# Response Function
-dwi2response tournier dwi_subset.mif response.txt \
--voxels response_voxels.mif \
--mask dwi_brain_mask.mif \
--force &&
-
-# FOD Estimation
-dwi2fod csd dwi_subset.mif response.txt fod.mif \
--mask dwi_brain_mask.mif \
+dwi2fod \
+csd \
+"${dir}/data_input.nii.gz" \
+"${dir}/response.txt" \
+"${dir}/fod.nii.gz" \
 -lmax 4 \
--force &&
+-mask "${dir}/dwi_brain_mask.nii.gz" \
+-fslgrad "${dir}/bvecs_input" "${dir}/bvals_input" &&
 
-# Intensity Normalization
-mtnormalise fod.mif fod_norm.mif \
--mask dwi_brain_mask.mif \
--force
-
-################################################################################
-
-# Brain Mask
-dwi2mask dwi_denoised_pe_corr.mif dwi_brain_mask.mif \
--grad grad_corr \
--force &&
-
-# Response Function
-dwi2response tournier dwi_denoised_pe_corr.mif response.txt \
--voxels response_voxels.mif \
--mask dwi_brain_mask.mif \
--grad grad_corr \
--force &&
-
-# FOD Estimation
-dwi2fod csd dwi_denoised_pe_corr.mif response.txt fod.mif \
--lmax 4 \
--mask dwi_brain_mask.mif \
--grad grad_corr \
--force &&
-
-# Intensity Normalization
-mtnormalise fod.mif fod_norm.mif \
--mask dwi_brain_mask.mif \
--force
+mtnormalise \
+"${dir}/fod.nii.gz" \
+"${dir}/fod_norm.nii.gz" \
+-mask "${dir}/dwi_brain_mask.nii.gz"
