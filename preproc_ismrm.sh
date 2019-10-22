3cfc5363a23e7b785c90d60ec115e11b73cfeaf2
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-10-22 10:32:52 +0200

    Add resizing for ismrm preproc

--- preproc_ismrm.sh
+++ preproc_ismrm.sh
@@ -66,7 +66,7 @@ then
     "${dir}/data_input.nii.gz" \
     -fslgrad "${dir}/bvecs_input" "${dir}/bvals_input" &&
 
-    rm -r "dwi"*"-tmp-"*
+    rm -r "dwi"*"-tmp-"* &&
 fi
 
 # Ground Truth ISMRM ###########################################################
@@ -108,7 +108,7 @@ then
     "${dir}/data.nii.gz" \
     "${dir}/dwi_wm_mask.nii.gz" \
     "${dir}/data_input.nii.gz" \
-    -fslgrad "${dir}/bvecs_input" "${dir}/bvals_input"
+    -fslgrad "${dir}/bvecs_input" "${dir}/bvals_input" &&
 fi
 
 # Reverse Phase ISMRM ##########################################################
@@ -182,5 +182,30 @@ then
     "${dir}/data_input.nii.gz" \
     -fslgrad "${dir}/bvecs_input" "${dir}/bvals_input" &&
 
-    rm -r "dwi"*"-tmp-"*
+    rm -r "dwi"*"-tmp-"* &&
 fi
+
+# Same for all ISMRM versions ##################################################
+
+mrresize \
+"${dir}/fa.nii.gz" \
+"${dir}/fa_125.nii.gz" \
+-voxel 1.25 &&
+
+mrresize \
+"${dir}/vec.nii.gz" \
+"${dir}/vec_125.nii.gz" \
+-voxel 1.25 &&
+
+cp "scoring/scoring_data/masks/wm.nii.gz" "${dir}/wm_mask.nii.gz"
+cp "scoring/scoring_data/masks/all_rois.nii.gz" "${dir}/all_rois.nii.gz"
+
+mrresize \
+"${dir}/wm_mask.nii.gz" \
+"${dir}/wm_mask_125.nii.gz" \
+-voxel 1.25 &&
+
+mrresize \
+"${dir}/all_rois.nii.gz" \
+"${dir}/all_rois_125.nii.gz" \
+-voxel 1.25 &&
