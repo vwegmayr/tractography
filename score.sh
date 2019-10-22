3ae416d287b55d24bf045984eb0b0788d9300c59
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-10-22 13:49:57 +0200

    Update scoring script

--- score.sh
+++ score.sh
@@ -3,19 +3,13 @@
 # Usage: ./score.sh trkdir
 
 trkfile="${1}/fibers.trk"
-alignedfile="${1}/fibers_tm.trk"
 
-python "../tractconverter/scripts/TractConverter.py" \
--i  $trkfile \
--o  $alignedfile \
--a "scoring_data/masks/wm2mm.nii.gz" &&
+python "scoring/validate_tracts_space.py" $trkfile &&
 
-python "validate_tracts_space.py" $alignedfile &&
-
-python "scripts/score_tractogram.py" \
-$alignedfile \
-"scoring_data" \
-$1 \
+python "scoring/scripts/score_tractogram.py" \
+$trkfile \
+--base_dir "scoring/scoring_data" \
+--out_dir $1 \
 --save_full_vc \
 --save_full_ic \
 --save_full_nc \
