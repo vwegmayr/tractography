7016c1df9e5476ccf833465ad39e0996e0c8c4fd
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-10-23 16:07:44 +0200

    Fix small bug with outdir

--- score.sh
+++ score.sh
@@ -2,14 +2,14 @@
 
 # Usage: ./score.sh trk_path
 
-python "scoring/validate_tracts_space.py" $1 &&
-
 out_dir="$(dirname $1)"
+
+python "scoring/validate_tracts_space.py" $1 &&
 
 python "scoring/scripts/score_tractogram.py" \
 $1 \
 --base_dir "scoring/scoring_data" \
---out_dir out_dir \
+--out_dir $out_dir \
 --save_full_vc \
 --save_full_ic \
 --save_full_nc \
