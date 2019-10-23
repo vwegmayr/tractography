a6564beace32c70bf19451f75735fa29745a7e85
Ami <abahrein@ethz.ch>
2019-10-23 17:40:28 +0200

    complete pipeline for both hcp and ismrm

--- prepare_hcp.sh
+++ prepare_hcp.sh
@@ -11,20 +11,36 @@
 # Additional Requirements:
 # MRtrix installation
 
-keep_merge=0.2
+keep_frac=0.2
+ext_num=020   # 100 * keep_frac - must be 3 digits integer
+merge_out_dir="merged_tracts"
+
 regex="hcp_zips/([0-9]+)_3T_Diffusion_preproc.zip"
 
 for fileadress in "hcp_zips/"*".zip"; do
     if [[ "${fileadress}" =~ $regex ]]
     then
         filename="${BASH_REMATCH[1]}"
+        
         echo "Unpacking ${filename} dwi files..." &&
         ./unpack_hcp_dwi.sh $filename &&
+        
         echo "Unpacking ${filename} track files..." &&
         ./unpack_hcp_trks.sh $filename &&
-        echo "Mergin ${filename} track files with keep ${keep_merge}..." &&
-        ./merge_hcp_trks.sh $filename $keep_merge
+        
+        echo "Mergin ${filename} track files with keep ${keep_frac}..." &&
+        python merge_tracks.py "subjects/${filename}/tracts" \
+        --keep "${keep_frac}" \
+        --weighted \
+        --out_dir "subjects/${filename}/${merge_out_dir}" &&
+        
+        echo "Resampling ${filename} track files with keep ${keep_merge}..." &&
+        python resample_trk.py "subjects/${filename}/${merge_out_dir}/merged_W${ext_num}.trk" &&
+        
+        echo "Estimating FOD for ${filename}..." &&
+        ./est_fod ${filename}
+        
     else
-        echo "WARNING: ${fileadress} is not a HCP folder. Skipped."
+        echo "WARNING: ${fileadress} is not a HCP folder. Skipped from the entire pipeline."
     fi
 done
