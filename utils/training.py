e1ef6c415f3c7c9dd7660db19630527315c60442
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-11-01 10:11:23 +0100

    Add scatter plot summaries

--- utils/training.py
+++ utils/training.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from matplotlib import pyplot as plt
+import matplotlib.colors as colors
 
 from tensorflow.python.eager import context
 from tensorflow.python.ops import summary_ops_v2
@@ -154,30 +155,64 @@ class FvMHybridSamples(FvMSamples):
 
 class TBSummaries(TensorBoard):
 
-    def __init__(self, *args, eval_seq=None, activations=None,
-        activations_freq=0, **kwargs):
+    def __init__(self, *args,
+        eval_seq=None,
+        activations_freq=0,
+        activations=None,
+        scatter=None,
+        scatter_frequ=0,
+        **kwargs):
         super(TBSummaries, self).__init__(*args, **kwargs)
 
-        if activations_freq > 0 and activations is None:
-            raise ValueError("activations must be set for activations_freq > 0")
+        if activations_freq and activations is None:
+            raise ValueError("activations must be set for activations_freq != 0")
+
+        if scatter_frequ and scatter is None:
+            raise ValueError("scatter must be set for scatter_frequ != 0")
 
         self.activations_freq = activations_freq
         self.activations = activations
+
+        self.scatter = scatter
+        self.scatter_frequ = scatter_frequ
+
         self.eval_seq = eval_seq
 
     def set_model(self, model):
         super(TBSummaries, self).set_model(model)
-        if self.activations_freq > 0:
+
+        if self.activations_freq:
             outputs = [model.get_layer(name).output for name in self.activations]
             self.activation_model = Model(model.input, outputs)
 
-    def on_epoch_end(self, epoch, logs=None):
+        if self.scatter_frequ:
+            x = [model.get_layer(name[0]).output for name in self.scatter]
+            y = [model.get_layer(name[1]).output for name in self.scatter]
+            self.scatter_x_model = Model(model.input, x)
+            self.scatter_y_model = Model(model.input, y)
+
+    def on_train_batch_end(self, batch, logs={}):
+        super(TBSummaries, self).on_train_batch_end(batch, logs)
+
+        if (self.activations_freq and self.activations_freq != "epoch" and
+            self._total_batches_seen % self.activations_freq == 0):
+            self._log_activations(self._total_batches_seen)
+
+        if (self.scatter_frequ and self.scatter_frequ != "epoch" and
+            self._total_batches_seen % self.scatter_frequ == 0):
+            self._scatter(self._total_batches_seen, logs)
+
+    def on_epoch_end(self, epoch, logs={}):
         """Runs metrics and histogram summaries at epoch end."""
         super(TBSummaries, self).on_epoch_end(epoch, logs)
-        if self.activations_freq and epoch % self.activations_freq == 0:
+
+        if self.activations_freq and self.activations_freq == "epoch":
             self._log_activations(epoch)
 
-    def _log_activations(self, epoch):
+        if self.scatter_frequ and self.scatter_frequ == "epoch":
+            self._scatter(epoch, logs)
+
+    def _log_activations(self, step):
         """Logs the outputs of the Model to TensorBoard."""
         writer = self._get_writer(self._train_run_name)
         with context.eager_mode(), writer.as_default(), \
@@ -186,28 +221,67 @@ class TBSummaries(TensorBoard):
             activations = self.activation_model.predict_generator(self.eval_seq)
             if not isinstance(activations, list):
                 activations = [activations]
+
             for i, values in enumerate(activations):
                 name = self.activations[i]
-                summary_ops_v2.histogram(name, values, step=epoch)
+                summary_ops_v2.histogram(name, values, step=step)
                 summary_ops_v2.scalar(name+"_mean",
-                    np.mean(values), step=epoch)
+                    np.mean(values), step=step)
             writer.flush()
+
+    def _scatter(self, epoch, logs={}):
+        x = self.scatter_x_model.predict_generator(self.eval_seq)
+        y = self.scatter_y_model.predict_generator(self.eval_seq)
+        # =====================================================================
+        writer = self._get_writer(self._train_run_name)
+        with context.eager_mode(), writer.as_default(), \
+            summary_ops_v2.always_record_summaries():
+            # ------------------------------------------------------------------
+            for i, name in self.scatter:
+                fig, ax = plt.subplots()
+                ax.hist2d(x[i], y[i], bins=50, density=True,
+                    norm=colors.SymLogNorm(linthresh=0.01, linscale=2, vmin=-1.0,
+                        vmax=2.0))
+                ax.set_xlabel(name[0])
+                ax.set_ylabel(name[1])
+                fig.canvas.draw()
+                # ------------------------------------------------------------------
+                plot = np.array(fig.canvas.renderer.buffer_rgba())
+                plot = np.expand_dims(plot, 0)
+                summary_ops_v2.image("2DHistogram", plot, step=step)
+                # ------------------------------------------------------------------
+                plt.close()
+                writer.flush()
 
 
 class FvMSummaries(TBSummaries):
 
-    def __init__(self, *args, activations=["kappa"], activations_freq=1, **kwargs):
-        super(FvMSummaries, self).__init__(*args, activations=activations,
-            activations_freq=activations_freq, **kwargs)
+    def __init__(self, *args,
+        eval_seq=None,
+        activations_freq="epoch",
+        activations=["kappa"],
+        **kwargs):
+        super(FvMSummaries, self).__init__(*args,
+            eval_seq=eval_seq,
+            activations=activations,
+            activations_freq=activations_freq,
+            **kwargs)
 
 
 class FvMHybridSummaries(TBSummaries):
 
-    def __init__(self, *args, activations=["kappa", "mu", "isterminal"], activations_freq=1, **kwargs):
-        super(FvMHybridSummaries, self).__init__(*args, activations=activations,
-            activations_freq=activations_freq, **kwargs)
+    def __init__(self, *args,
+        eval_seq=None,
+        activations_freq="epoch",
+        activations=["kappa", "mu", "isterminal"],
+        **kwargs):
+        super(FvMHybridSummaries, self).__init__(*args,
+            eval_seq=eval_seq,
+            activations_freq=activations_freq,
+            activations=activations,
+            **kwargs)
 
-    def _log_activations(self, epoch):
+    def _log_activations(self, step):
         writer = self._get_writer(self._train_run_name)
         with context.eager_mode(), writer.as_default(), \
             summary_ops_v2.always_record_summaries():
@@ -219,33 +293,33 @@ class FvMHybridSummaries(TBSummaries):
             # ==================================================================
             kappa = activation_values[0]
             summary_ops_v2.histogram("kappa_midway", kappa[midway],
-                step=epoch)
+                step=step)
             summary_ops_v2.scalar("kappa_midway_mean",
-                np.mean(kappa[midway]), step=epoch)
+                np.mean(kappa[midway]), step=step)
             # ------------------------------------------------------------------
             summary_ops_v2.histogram("kappa_terminal", kappa[terminal],
-                step=epoch)
+                step=step)
             summary_ops_v2.scalar("kappa_terminal_mean",
-                np.mean(kappa[terminal]), step=epoch)
+                np.mean(kappa[terminal]), step=step)
             # ==================================================================
             mu = activation_values[1]
             neg_dot_prod_midway = -np.sum(
                 mu[midway] * self.eval_seq.outgoing[midway], axis=1)
             summary_ops_v2.histogram("neg_dot_prod_midway", neg_dot_prod_midway,
-                step=epoch)
+                step=step)
             summary_ops_v2.scalar("neg_dot_prod_midway_mean",
-                np.mean(neg_dot_prod_midway), step=epoch)
+                np.mean(neg_dot_prod_midway), step=step)
             # ------------------------------------------------------------------
             neg_dot_prod_terminal = -np.sum(
                 mu[terminal] * self.eval_seq.outgoing[terminal], axis=1)
             summary_ops_v2.histogram("neg_dot_prod_terminal",
-                neg_dot_prod_terminal, step=epoch)
+                neg_dot_prod_terminal, step=step)
             summary_ops_v2.scalar("neg_dot_prod_terminal_mean",
-                np.mean(neg_dot_prod_terminal), step=epoch)
+                np.mean(neg_dot_prod_terminal), step=step)
             # ==================================================================
             isterminal = activation_values[2]
             ave_prec = average_precision_score(self.eval_seq.isterminal, isterminal)
-            summary_ops_v2.scalar("average_precision", ave_prec, step=epoch)
+            summary_ops_v2.scalar("average_precision", ave_prec, step=step)
             # ------------------------------------------------------------------
             precision, recall, thresh = precision_recall_curve(
                 y_true=self.eval_seq.isterminal,
@@ -262,15 +336,72 @@ class FvMHybridSummaries(TBSummaries):
             # ------------------------------------------------------------------
             plot = np.array(fig.canvas.renderer.buffer_rgba())
             plot = np.expand_dims(plot, 0)
-            summary_ops_v2.image("Precision-Recall", plot, step=epoch)
+            summary_ops_v2.image("Precision-Recall", plot, step=step)
             # ==================================================================
+            plt.close()
+            writer.flush()
+
+
+class EntrackSummaries(TBSummaries):
+
+    def __init__(self, *args,
+                 eval_seq=None,
+                 activations_freq="epoch",
+                 activations=["kappa"],
+                 scatter=[("kappa", "mu")],
+                 scatter_frequ="epoch",
+                 **kwargs
+        ):
+        super(EntrackSummaries, self).__init__(*args,
+            eval_seq=eval_seq,
+            activations_freq=activations_freq,
+            activations=activations,
+            scatter=scatter,
+            scatter_frequ=scatter_frequ,
+            **kwargs
+        )
+
+    def _scatter(self, step, logs):
+        kappa_pred = self.scatter_x_model.predict_generator(self.eval_seq)
+        mu_pred = self.scatter_y_model.predict_generator(self.eval_seq)
+
+        mu_true = self.eval_seq.outgoing
+
+        agreement = np.sum(mu_true * mu_pred, axis=1)
+        kappa_mean = kappa_pred.mean() + 10**-9
+        kappa_pred /= kappa_mean
+
+        # =====================================================================
+        writer = self._get_writer(self._train_run_name)
+        with context.eager_mode(), writer.as_default(), \
+            summary_ops_v2.always_record_summaries():
+            #summary_ops_v2.histogram("kappa", kappa_pred, step=step)
+            #summary_ops_v2.scalar("kappa_mean", np.mean(kappa_pred), step=step)
+            # ------------------------------------------------------------------
+            fig, ax = plt.subplots()
+            ax.hist2d(kappa_pred, agreement, bins=50, density=True,
+                norm=colors.SymLogNorm(linthresh=0.01, linscale=2, vmin=-1.0,
+                    vmax=2.0))
+            ax.plot([0, 1], [0, 1/(logs["T"]+10**-9)/kappa_mean])
+            ax.set_xlabel("Certainty k")
+            ax.set_ylabel("Agreement <m,v>")
+            fig.canvas.draw()
+            # ------------------------------------------------------------------
+            plot = np.array(fig.canvas.renderer.buffer_rgba())
+            plot = np.expand_dims(plot, 0)
+            summary_ops_v2.image("2DHistogram", plot, step=step)
+            # ------------------------------------------------------------------
             plt.close()
             writer.flush()
 
 
 class RNNSummaries(TensorBoard):
 
-    def __init__(self, *args, eval_seq=None, activations=None, activations_freq=0, **kwargs):
+    def __init__(self, *args,
+        eval_seq=None,
+        activations=None,
+        activations_freq=0,
+        **kwargs):
         super(RNNSummaries, self).__init__(*args, **kwargs)
 
         if activations_freq > 0 and activations is None:
@@ -347,6 +478,14 @@ class ConstantTemperatureSchedule(Callback):
         else:
             logs = {"T": t}
 
+    def on_train_batch_end(self, batch, logs={}):
+        self.step += 1
+        t = self.schedule(self.step)
+        if logs is not None:
+            logs.update({"T": t}) 
+        else:
+            logs = {"T": t}
+
     def on_epoch_begin(self, epoch, logs={}):
         t = self.schedule(self.step)
         if logs is not None:
@@ -354,8 +493,12 @@ class ConstantTemperatureSchedule(Callback):
         else:
             logs = {"T": t}
 
-    def on_train_batch_end(self, batch, logs={}):
-        self.step += 1
+    def on_epoch_end(self, epoch, logs={}):
+        t = self.schedule(self.step)
+        if logs is not None:
+            logs.update({"T": t})
+        else:
+            logs = {"T": t}
 
     def get_config(self):
         return {"name": self.name,
@@ -409,6 +552,49 @@ class HarmonicTemperatureSchedule(ConstantTemperatureSchedule):
         config.update({
             "T_start": self.T_start,
             "T_end": self.T_end,
+            "n_steps": self.n_steps,
+            "name": self.name
+            })
+        return config
+
+
+class LinearTemperatureScheduleWithWarmup(ConstantTemperatureSchedule):
+    """docstring for PiecewiseConstantTemperature"""
+    def __init__(self, T_start, T_warmup, T_end, n_warmup_steps, n_steps, n_wait_steps=0,
+        **kwargs):
+        self.T_start = float(K.get_value(T_start))
+        self.T_warmup = T_warmup
+        self.T_end = T_end
+        self.n_warmup_steps = n_warmup_steps
+        self.n_wait_steps = n_wait_steps
+        self.n_steps = n_steps
+
+        if self.n_warmup_steps:
+            self.warmup_rate = (self.T_warmup - self.T_start) / self.n_warmup_steps
+        else:
+            self.warmup_rate = 0
+        self.cooling_rate = (self.T_end - self.T_warmup) / (self.n_steps - 
+            self.n_warmup_steps - self.n_wait_steps)
+
+        super(LinearTemperatureScheduleWithWarmup, self).__init__(T_start, **kwargs)
+
+    def schedule(self, step):
+
+        if step < self.n_wait_steps:
+            return self.T_start
+        elif step < self.n_warmup_steps:
+            return self.T_start + self.warmup_rate * (step - self.n_wait_steps)
+        else:
+            return self.T_warmup + self.cooling_rate * (step - 
+                self.n_warmup_steps - self.n_wait_steps) 
+
+    def get_config(self):
+        config = super(LinearTemperatureScheduleWithWarmup, self).get_config()
+        config.update({
+            "T_start": self.T_start,
+            "T_warmup": self.T_warmup,
+            "T_end": self.T_end,
+            "n_warmup_steps": self.n_warmup_steps,
             "n_steps": self.n_steps,
             "name": self.name
             })
