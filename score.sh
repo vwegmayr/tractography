fbe529dcef7e749e2c1793e0ef0dc8d2c844f40d
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-11-05 14:25:39 +0100

    Add prefixing for e.g. scoring same file with different shifts

--- score.sh
+++ score.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Usage: ./score.sh trk_path
+# Usage: ./score.sh trk_path prefix
 
 out_dir="$(dirname $1)"
 
 python "scoring/validate_tracts_space.py" $1 &&
 
+mv $1 "${2}_${1}"
+
 python "scoring/scripts/score_tractogram.py" \
-$1 \
+"${2}_${1}" \
 --base_dir "scoring/scoring_data" \
 --out_dir $out_dir \
 --save_full_vc \
@@ -16,3 +18,5 @@ $1 \
 --save_ib \
 --save_vb \
 -v
+
+mv "${2}_${1}" $1
