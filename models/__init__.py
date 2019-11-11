321f6580450d8ad0b64b06d3f43cfbf50585bbc3
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2019-11-11 16:34:13 +0100

    Add detrack model

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
-from .model_classes import FvM, FvMHybrid, RNNGRU, Entrack, RNNLSTM
+from .model_classes import FvM, FvMHybrid, RNNGRU, Entrack, RNNLSTM, Detrack
 
 MODELS = {"FvM": FvM,
+          "Detrack": Detrack,
           "FvMHybrid": FvMHybrid,
           "RNNGRU": RNNGRU,
           "Entrack": Entrack,
