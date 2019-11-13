ced5d88abfde9abcc21081091e80bb59c8ec6ce4
Ami <abahrein@ethz.ch>
2019-11-13 23:38:35 +0100

    bug fix

--- inference.py
+++ inference.py
@@ -359,8 +359,7 @@ def run_rnn_inference(config, gpu_queue=None):
     model_config = {'batch_size': batch_size,
                     'input_shape':  trained_model.input_shape[1:],
                     'temperature': 0.04}
-    modelClass = MODELS[model_name](model_config)
-    prediction_model = modelClass(model_config).keras
+    prediction_model = MODELS[model_name](model_config).keras
     prediction_model.set_weights(trained_model.get_weights())
 
     terminator = Terminator(config['term_path'], config['thresh'])
