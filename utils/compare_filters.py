834b6643c707fbe2e44608268bb9d29880e6007e
Ami <abahrein@ethz.ch>
2019-11-26 15:26:23 +0100

    compare filtering thresholds and criteria by plotting

---
+++ utils/compare_filters.py
@@ -0,0 +1,76 @@
+from os.path import join, isdir
+from os import listdir
+import argparse
+import json
+
+import matplotlib.pyplot as plt
+
+SCORES = ['mean_F1', 'IC', 'IB', 'VC', 'VB']
+BASELINES = [0.47369345142021646, 0.4257722385427191, 116, 0.5372817904136711, 24]
+FILTERS = ["log_prob_ratio", "log_prob_sum", "log_prob"]
+
+
+def compare(results_path, percentiles, filters):
+    for i, score in enumerate(SCORES):
+        baseline = BASELINES[i]
+        compare_score(results_path, percentiles, filters, score, baseline)
+
+
+def compare_score(results_path, percentiles, filters, score_name='mean_F1',
+                  baseline=0.47369345142021646):
+
+    assert all(percentiles[i] <= percentiles[i+1]
+               for i in range(len(percentiles)-1)), 'percentiles must be sorted'
+
+    # Add one list for each filtering criteria
+    criteria_scores = {'baseline': []}
+    for criteria in filters:
+        criteria_scores[criteria] = []
+
+    # Append values from json to each list
+    for percentile in percentiles:
+        for criteria in filters:
+            scoring_dir = join(results_path, f"scorings_p_{percentile}-f_{criteria}")
+            if not isdir(scoring_dir):
+                raise FileNotFoundError
+
+            scoring_dir = join(scoring_dir, "scores")
+            json_path = [file for file in listdir(scoring_dir)
+                         if file.endswith('.json')]
+
+            # Un comment for local use!
+            # json_path = join(results_path, f'{criteria}_{percentile}_k=f.json')
+
+            with open(json_path) as json_file:
+                scores = json.load(json_file)
+
+            criteria_scores[criteria].append(scores[score_name])
+        criteria_scores['baseline'].append(baseline)
+
+    fig, ax = plt.subplots()
+    for key, values in criteria_scores.items():
+        if len(values) > 0:
+            if key == 'baseline':
+                ax.plot(percentiles, values, label=key, linestyle='dashed')
+            else:
+                ax.plot(percentiles, values, label=key)
+    legend = ax.legend()
+    plt.title(f'{score_name}')
+    fig_path = join(results_path, f'compare_filter_{score_name}.png')
+    print(f'Saving plot to {fig_path}')
+    plt.savefig(fig_path)
+
+    pass
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Filter unlikely fibers.")
+    parser.add_argument("results_path", type=str)
+    parser.add_argument('--percentiles', nargs='+', type=int, default=[],
+                        help="list of percentiles to try")
+    parser.add_argument('--criteria', nargs='+', type=str, default=FILTERS,
+                        help="list of criteria to try")
+    args = parser.parse_args()
+
+    compare(args.results_path, args.percentiles, args.criteria)
